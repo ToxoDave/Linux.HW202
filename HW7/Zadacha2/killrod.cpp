3d38a49878cab45d4d3b57b994023c564f71c82c
David <baludavo@gmail.com>
2023-11-18 01:47:07 +0400

    HWs

---
+++ HW7/Zadacha2/killrod.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <cerrno>
+#include <signal.h>
+#include <sys/wait.h>
+
+int main()
+{
+	int status;
+	pid_t fk = fork();
+	if(fk == -1)
+	{
+		perror("Cant fork");
+		exit(errno);
+	}
+	if(fk == 0)
+	{
+		int id = getppid();
+		if(kill(id, SIGKILL) < 0)
+		{
+			perror("Cant kill");
+			exit(errno);
+		}
+		else {
+			std::cout << "Parent killed x_x" << std::endl;
+		}
+	}
+
+
+
+	return 0;
+}
